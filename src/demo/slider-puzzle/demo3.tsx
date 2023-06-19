9d977ffc382f49dfcae46ae7c849bf9cb8d8fc15
lhh <970519495@qq.com>
2023-06-19 17:34:34 +0800

    fix: 完善部分导入导出

--- src/demo/slider-puzzle/demo3.tsx
+++ src/demo/slider-puzzle/demo3.tsx
@@ -1,29 +1,29 @@
-import React from "react"
-import { SliderPuzzle, isMobile } from "lhh-ui"
+import { isMobile, SliderPuzzle } from 'lhh-ui';
+import React from 'react';
 
 export default () => {
   return (
-    <SliderPuzzle 
+    <SliderPuzzle
       // 数组的长度需要设置
-      listLength={0} 
+      listLength={0}
       // 宽高必须设置
-      style={{width: '300px', height: '300px'}}
-      size={5}
+      style={{ width: '300px', height: '300px' }}
+      size={3}
       gap={0}
       globalAlpha={1}
-      puzzleImg='http://lhh.codeape.site/img/tom.jpeg'
+      puzzleImg="http://lhh.codeape.site/img/tom.jpeg"
       onComplete={() => {
-        alert('恭喜你完成了拼图')
+        alert('恭喜你完成了拼图');
       }}
     >
       {/* 这里是完整的拼图 */}
-      <SliderPuzzle.Canvas 
+      <SliderPuzzle.Canvas
         style={{
           position: 'absolute',
           top: isMobile ? -100 : 0,
-          left: isMobile ? 0 : 320, 
-        }} 
+          left: isMobile ? 0 : 320,
+        }}
       />
     </SliderPuzzle>
-  )
-}
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-// components
 export * from './floating-ball';
-// hooks
 export * from './hooks/use-latest';
 export * from './hooks/use-merge-props';
 export * from './hooks/use-render';
@@ -8,5 +6,4 @@ export * from './hooks/use-touch';
 export * from './hooks/use-touch-event';
 export * from './scroll-circle';
 export * from './slider-puzzle';
-// utils
 export * from './utils/handleDom';
